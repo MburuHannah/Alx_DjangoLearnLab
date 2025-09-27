7121a98b84a5a855d8b25c4a0f8a3a5be90db421
MburuHannah <hannahmburu897@gmail.com>
2025-09-27 15:09:20 +0300

    Save local changes before rebase

---
+++ LibraryProject/bookshelf/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    title =models.CharField(max_length=200)
+    author=models.CharField(max_length=100)
+    publication_year=models.IntegerField()
+
+def __str__(self):
+    return self.title
+def get_author(self):
+    return self.author
+def get_publication_year     (self):
+    return self.publication_year
+
+
+
